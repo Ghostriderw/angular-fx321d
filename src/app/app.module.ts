474b347672884f39a9e25c66b6aff57783c14bc6
Ghostriderw <alexmonin@orange.fr>
2019-10-31 11:33:12 +0100

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MonPremierComponent } from './mon-premier/mon-premier.component';
+import { AppareilComponentComponent } from './appareil-component/appareil-component.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, MonPremierComponent ],
+  declarations: [ AppComponent, MonPremierComponent, AppareilComponentComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/appareil-component/appareil-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appareil-component',
+  templateUrl: './appareil-component.component.html',
+  styleUrls: ['./appareil-component.component.css']
+})
+export class AppareilComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
