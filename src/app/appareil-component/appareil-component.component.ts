7b02bb19bdfd2d464cbfb37486893d3affa5aa8a
Ghostriderw <alexmonin@orange.fr>
2019-11-03 21:47:48 +0100

    1 file modified

--- src/app/appareil-component/appareil-component.component.ts
+++ src/app/appareil-component/appareil-component.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { appareilService } from './services/appareil.service';
+import { AppareilService } from '../services/appareil.service';
 
 @Component({
   selector: 'app-appareil-component',
