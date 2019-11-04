e308250ca2ae8a1cda3de1f619874402b2a2b31b
Ghostriderw <alexmonin@orange.fr>
2019-11-04 17:42:49 +0100

    4 files modified, 5 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,40 +7,5 @@ import { AppareilService } from './services/appareil.service';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  title = 'Avec Alex On APEX';
-  isAuth = false;
-  lastUpdate = new Promise((resolve, reject) => {
-    const date = new Date();
-    setTimeout(
-      () => {
-        resolve(date);
-      }, 2000
-    );
-  });
-
-  appareils: any[];
-  
-  constructor(private appareilService: AppareilService) {
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 4000
-    );
-  }
-
-  ngOnInit() {
-    this.appareils = this.appareilService.appareils;
-  }
-  
-  onAllumer() {
-    this.appareilService.switchOnAll();
-  }
-
-  onEteindre() {
-    if(confirm('Etes-vous sûr de vouloir vider tous les chargeurs ?')) {
-      this.appareilService.switchOffAll();
-    } else {
-      return null;
-    }
-}
+  constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { MonPremierComponent } from './mon-premier/mon-premier.component';
 import { AppareilComponent } from './appareil-component/appareil-component.component';
 import { AppareilService } from './services/appareil.service';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, MonPremierComponent, AppareilComponent ],
+  declarations: [ AppComponent, MonPremierComponent, AppareilComponent, AppareilViewComponent, AuthComponent ],
   bootstrap:    [ AppComponent ],
   providers: [AppareilService]
 })
